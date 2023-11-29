e65da2cf1f1e537afdbdcf96c632ae1bf3794daa
BLACKMARKSTUDIO <142732531+BLACKMARKSTUDIO@users.noreply.github.com>
2023-11-29 21:17:00 +0900

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,27 +1,25 @@
 <?php
-// 업로드된 파일 처리
-if(isset($_FILES['file'])) {
-    $file = $_FILES['file'];
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["file"])) {
+    $file = $_FILES["file"];
 
     // 파일 정보 확인
-    $fileName = $file['name'];
-    $fileType = $file['type'];
-    $fileSize = $file['size'];
-    $fileTmpName = $file['tmp_name'];
-    $fileError = $file['error'];
+    $fileName = $file["name"];
+    $fileType = $file["type"];
+    $fileSize = $file["size"];
+    $fileTmpName = $file["tmp_name"];
+    $fileError = $file["error"];
+
+    // 파일 업로드 디렉토리
+    $uploadDirectory = "uploads/";
+
+    // 파일을 지정된 디렉토리로 이동
+    $destination = $uploadDirectory . $fileName;
 
     // 파일 확장자 확인 (예: 허용된 확장자가 .jpg, .png 인 경우)
     $allowedExtensions = array("jpg", "png");
     $fileExtension = pathinfo($fileName, PATHINFO_EXTENSION);
 
     if (in_array($fileExtension, $allowedExtensions)) {
-        // 파일 업로드 디렉토리
-        $uploadDirectory = "uploads/";
-
-        // 파일을 지정된 디렉토리로 이동
-        $newFileName = uniqid('', true) . "." . $fileExtension; // 파일명 중복 방지
-        $destination = $uploadDirectory . $newFileName;
-
         if (move_uploaded_file($fileTmpName, $destination)) {
             echo "파일 업로드 성공!";
         } else {
@@ -32,14 +30,3 @@ if(isset($_FILES['file'])) {
     }
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>업로드 결과</title>
-</head>
-<body>
-    <h1>파일 업로드 결과</h1>
-    <!-- 업로드 결과 또는 추가 정보를 표시하는 부분 -->
-</body>
-</html>
